223d8d9d0cd4a1d5a481f0168b4aaf7b7936522d
Yeganeh <vnus.daneshparvar@gmail.com>
2024-04-07 22:57:06 -0400

    back to localhost

--- src/app/graphql/graphql.provider.ts
+++ src/app/graphql/graphql.provider.ts
@@ -2,8 +2,9 @@ import { Apollo, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { ApplicationConfig, inject } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
-
-const uri = 'https://101383730-comp-3133-assignment1-fnu4.vercel.app/';
+// 'https://101383730-comp-3133-assignment1-xzxn.vercel.app/'
+//'http://localhost:4000/graphql ';
+const uri = 'http://localhost:4000/graphql ';
 export function apolloOptionsFactory(): ApolloClientOptions<any> {
   const httpLink = inject(HttpLink);
   return {
