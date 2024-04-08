33602552304d6868ac6065f8934287cd661331e6
Yeganeh <vnus.daneshparvar@gmail.com>
2024-04-07 22:39:31 -0400

    new commit

--- src/app/graphql/graphql.provider.ts
+++ src/app/graphql/graphql.provider.ts
@@ -3,7 +3,7 @@ import { HttpLink } from 'apollo-angular/http';
 import { ApplicationConfig, inject } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 
-const uri = 'https://101383730-comp-3133-assignment1.vercel.app/';
+const uri = 'https://101383730-comp-3133-assignment1-fnu4.vercel.app/';
 export function apolloOptionsFactory(): ApolloClientOptions<any> {
   const httpLink = inject(HttpLink);
   return {
